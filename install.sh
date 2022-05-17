80321d9038ddc6921a9112fa41885f29a8c777af
solarkraft <1337470+solarkraft@users.noreply.github.com>
2022-05-17 22:37:03 +0200

    Basic installation script and playbook

--- install.sh
+++ install.sh
@@ -1,11 +1,42 @@
+#!/usr/bin/env sh
+
+### Install Ansible
+
 echo "Updating apt repositories ..."
 sudo apt update
 echo "Installing Ansible ..."
 # We might switch to the pip version if this one is too old
-sudo apt install -y ansible git
+sudo apt install -y ansible #git
 echo "Ansible installed: "
 ansible --version
 
-echo "Creating desktop icon ..."
-# tbd
-echo "Installation complete. To continue the configuration, click the new desktop icon. "
+### Install playbook
+
+cd /root
+sudo git clone https://github.com/fablab-luenen/provisioning/
+cd provisioning
+sudo git switch lab-pc
+sudo chown -R $USER .
+
+### Add desktop icon
+launcher=$HOME/Schreibtisch/pull.desktop
+echo "[Desktop Entry]
+Type=Application
+Version=1.0
+
+Name=Update configuration
+Name[de]=Konfiguration aktualisieren
+Icon=reload
+
+Exec=/root/provisioning/pull.sh
+Path=/root/provisioning
+
+Terminal=true" > $launcher
+chmod +x $launcher
+
+# Add to autostart
+cp $launcher $HOME/.config/autostart
+
+### Run playbook
+
+sudo ansible-playbook lab-pc.yaml
---
+++ pull.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if read -r -s -n 1 -t 3 -p "Pulling most recent Ansible config (press any key within 3 seconds to abort):" key
+then
+    echo "Alright. "; exit
+fi
+
+# I am aware of ansible-pull. It lacks the flexibility of allowing the editing of files without committing them - and this is pretty much all it does: 
+git pull
+ansible-playbook lab-pc.yaml
+
+read -n 1 -s -r -p "Done! Press any key to continue" && echo
